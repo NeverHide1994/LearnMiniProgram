7a63651d0b7b309628987662e0382aa72ee9b978
6233V陈忠其 <chenzq@bpg.com>
2020-01-07 14:44:37 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,21 @@
 Page({
-  
+  data:{
+    name:'卧槽',
+    movies:[
+      {id:1,name:'天生杀人狂',year:1994},
+      {id:2,name:'落水狗',year:1994},
+      {id:3,name:'阿甘正传',year:1994},
+    ],
+    counter:0
+  },
+  handleAdd(){
+    this.setData({
+      counter:this.data.counter +=1
+    })
+  },
+  handleSubtract(){
+    this.setData({
+      counter:this.data.counter -=1
+    })
+  }
 })
